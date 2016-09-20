8f2ff8b6ba650defa3459efb93d33b94945c4e50
Andy Knolla <andyknolla@gmail.com>
2016-09-20 11:54:02 -0600

    mod

--- modules/articles/client/config/articles.client.config.js
+++ modules/articles/client/config/articles.client.config.js
@@ -5,7 +5,7 @@ angular.module('articles').run(['Menus',
   function (Menus) {
     // Add the articles dropdown item
     Menus.addMenuItem('topbar', {
-      title: 'Goals',
+      title: 'Purchases',
       state: 'articles',
       type: 'dropdown',
       roles: ['*']
@@ -13,13 +13,13 @@ angular.module('articles').run(['Menus',
 
     // Add the dropdown list item
     Menus.addSubMenuItem('topbar', 'articles', {
-      title: 'List Goals',
+      title: 'List Purchases',
       state: 'articles.list'
     });
 
     // Add the dropdown create item
     Menus.addSubMenuItem('topbar', 'articles', {
-      title: 'Create Goal',
+      title: 'Record Purchase',
       state: 'articles.create',
       roles: ['user']
     });
