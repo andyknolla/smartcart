84a3c44a2db30bb685b269c06746906128a58718
Andy Knolla <andyknolla@gmail.com>
2016-09-24 16:55:25 -0600

    flex layout

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -4,5 +4,8 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
   function ($scope, Authentication) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
+    $scope.login = function() {
+      alert('You\'ll need to log in to save receipts.');
+    }
   }
 ]);
